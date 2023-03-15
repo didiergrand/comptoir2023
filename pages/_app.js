2030b805f2ee0c5e031aa9eb883a5ba7361ecb08
Didier.Grand <didier.grand@scott-sports.com>
2023-03-15 15:45:40 +0100

    add favicon to head

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,20 @@ import '@component/styles/pages/comite-direction.css'
 import '@component/styles/pages/programme.scss'
 import '@component/styles/footer.css'
 
+// import the Head component for appending elements to the head of the page
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      {/* Add the favicon */}
+      {/* Note that the path doesn't include "public" */}
+
+      <Component {...pageProps} />
+    </>
+  );
 }

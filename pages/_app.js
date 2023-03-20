bc330cf2c4dcdfa7854375c547f7f928a93e295c
Didier.Grand <didier.grand@scott-sports.com>
2023-03-20 10:14:06 +0100

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,46 @@ import '@component/styles/pages/homepage.css'
 import '@component/styles/pages/comite-direction.css'
 import '@component/styles/pages/programme.scss'
 
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
+
 // import the Head component for appending elements to the head of the page
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
